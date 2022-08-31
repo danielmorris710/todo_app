09d2a8a6b0afedbb9b051044866289808aa23bd8
Daniel Morris <uptownmelo#gmail.com>
2022-08-31 15:31:08 -0400

    added heroku URL to api

--- src/services/todo_api.js
+++ src/services/todo_api.js
@@ -1,32 +1,33 @@
 // this file will be our endpoint routes
 import axios from 'axios'
+const baseURL ='https://dam-express-api.herokuapp.com/todos'
 
 export const getTodos = () => {
-    const URL = 'http://localhost:3001/todos'
+    const URL = baseURL
     const response = axios.get(URL)
     return response
 }
 
 export const getTodo = (id) => {
-    const URL = `http://localhost:3001/todos/${id}`
+    const URL = `${baseURL}/${id}`
     const response = axios.get(URL) 
     return response
 }
 
 export const deleteTodo = (id) => {
-    const URL = `http://localhost:3001/todos/${id}`
+    const URL = `${baseURL}/${id}`
     const response = axios.delete(URL)
     return response
 }
 
 export const editTodo = (id, updatedTodo) => {
-    const URL = `http://localhost:3001/todos/${id}`
+    const URL = `${baseURL}/${id}`
     const response = axios.put(URL, updatedTodo)
     return response
 }
 
 export const createTodo = (todo) => {
-    const URL = `http://localhost:3001/todos`
+    const URL = baseURL
     const response = axios.post(URL, todo)
     return response
 }
